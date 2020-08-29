10487a318b12a04e93be3bd4d990263afc626bd9
William Freitag <william.h.freitag@gmail.com>
2020-08-29 12:50:07 -0700

    updated chat-app

--- ChatServer.js
+++ ChatServer.js
@@ -44,6 +44,7 @@ app.get('/chat.html',(req,res)=>res.sendFile(path.resolve(__dirname+'/chat.html'
 app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname+'/Login.html')));
 
 app.get('/Login.html',(req,res)=>{
+  console.log(req.session.user);
   if(req.session.user) res.redirect("/chat.html");
   else res.sendFile(path.resolve(__dirname+'/Login.html'));
 });
